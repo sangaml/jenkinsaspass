002c57f3dccba360e82e4cf3487cab187f25c986
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-01 22:34:10 +0530

    Update installjenkins.ps1

--- installjenkins.ps1
+++ installjenkins.ps1
@@ -3,5 +3,5 @@ Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/softwa
 cd D:\
 Start-Process jenkins.msi /qn -Wait
 $Password = Get-Content "C:\Program Files (x86)\Jenkins\secrets\initialAdminPassword"
-Write-Host "Login from Browser" -ForegroundColor Green  
-Write-Host "Initial Admin Password is "$Password"" -ForegroundColor Green
+#Write-Host "Login from Browser" -ForegroundColor Green  
+#Write-Host "Initial Admin Password is "$Password"" -ForegroundColor Green
