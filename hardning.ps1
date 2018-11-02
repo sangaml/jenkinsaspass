51d94bcbce9a57fded07717ce5a13638810b6b9c
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-02 19:23:40 +0530

    Create hardning.ps1

---
+++ hardning.ps1
@@ -0,0 +1,33 @@
+Configuration elkhardening {
+param
+   (
+   # Target nodes to apply the configuration
+   [string[]]$NodeName = 'localhost'
+   )
+# Import the module that defines custom resources
+   Import-DscResource -ModuleName PSDesiredStateConfiguration
+   Import-DscResource -ModuleName RemoteFileManagement
+
+Node $NodeName
+{
+   Registry 'DisableRunAs' {
+       Ensure    = 'Present'
+       Key       = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WinRM\Service'
+       ValueName = 'DisableRunAs'
+       ValueType = 'DWord'
+       ValueData = '1'
+   }
+   WindowsFeature 'Telnet-Client' {
+       Name   = 'Telnet-Client'
+       Ensure = 'Present'
+   }
+   Registry 'AdmPwdEnabled' {
+       Ensure    = 'Present'
+       Key       = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft Services\AdmPwd'
+       ValueName = 'AdmPwdEnabled'
+       ValueType = 'DWord'
+       ValueData = '1'
+   }
+
+}
+}
