22c5ce4419b7444dc34c4cf3719b00f6d7776989
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-02 17:36:41 +0530

    Update and rename installjenkins.ps1 to install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,8 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/jenkins.msi' `
 -Destination 'D:\'
 cd D:\
-Start-Process jenkins.msi /qn -Wait
+Start-Process jenkins.msi /qn 
+Start-Sleep 60
 $Password = Get-Content "C:\Program Files (x86)\Jenkins\secrets\initialAdminPassword"
 #Write-Host "Login from Browser" -ForegroundColor Green  
 #Write-Host "Initial Admin Password is "$Password"" -ForegroundColor Green
