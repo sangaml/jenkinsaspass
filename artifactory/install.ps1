366a8f08ada71a4cf1af183b22edd6c3a2dc0a93
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-04 15:05:57 +0530

    Update install.ps1

--- artifactory/install.ps1
+++ artifactory/install.ps1
@@ -1,13 +1,11 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
-Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip' `
--Destination 'D:\'
+  Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip' -Destination 'D:\'
 
-Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe' `
--Destination 'D:\'
+  Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe' -Destination 'D:\'
 
-$Exe = 'D:\jre-8u191-windows-x64.exe'
-$ExeArgs = @(
+  $Exe = 'D:\jre-8u191-windows-x64.exe'
+  $ExeArgs = @(
    "INSTALL_SILENT=1"
    "STATIC=0"
    "AUTO_UPDATE=0"
@@ -19,19 +17,19 @@ $ExeArgs = @(
    "NOSTARTMENU=0"
    "SPONSORS=0"
 )
-Start-Process $Exe -ArgumentList $ExeArgs -NoNewWindow -Wait 
+  Start-Process $Exe -ArgumentList $ExeArgs -NoNewWindow -Wait 
 
-Start-Sleep 60
+  Start-Sleep 60
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function unzip {
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  function unzip {
    param( [string]$ziparchive, [string]$extractpath )
    [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
 }
-unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
+  unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
 
-D:\artifactory-oss-6.5.2\bin\run.vbs
+  D:\artifactory-oss-6.5.2\bin\run.vbs
 
-Write-Host "Login from browser with port 8081" -ForegroundColor Green 
+  Write-Host "Login from browser with port 8081" -ForegroundColor Green 
 
-Write-Host "Login Username and PAssword is admin" -ForegroundColor Green
+  Write-Host "Login Username and PAssword is admin" -ForegroundColor Green
