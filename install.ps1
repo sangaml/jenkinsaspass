1f372b8a1a2f83bdd9ce4a876c7bc5d8ec62855e
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-09 12:23:20 +0530

    Delete install.ps1

--- install.ps1
+++
@@ -1,5 +0,0 @@
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-
-  Start-BitsTransfer -Source "https://jenkinsaspass.blob.core.windows.net/software/jenkins.msi" -Destination "D:\"
-  
-  Start-Process D:\jenkins.msi /qn
