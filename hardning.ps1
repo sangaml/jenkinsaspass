182bdf559503346745e8fd334dee8f6d0520c679
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-09 12:25:20 +0530

    Update hardning.ps1

--- hardning.ps1
+++ hardning.ps1
@@ -5,8 +5,7 @@ param
    [string[]]$NodeName = 'localhost'
    )
 # Import the module that defines custom resources
-   Import-DscResource -ModuleName PSDesiredStateConfiguration
-   Import-DscResource -ModuleName RemoteFileManagement
+   Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
 
 Node $NodeName
 {
