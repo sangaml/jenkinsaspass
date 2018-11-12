d16530a6cd4af299f3588e3c24c3f62e796fa27b
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-12 11:25:32 +0530

    Update installjava.ps1

--- installjava.ps1
+++ installjava.ps1
@@ -1,5 +1,5 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Start-BitsTransfer -Source "https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe" -Destination "D:\"
+Start-BitsTransfer -Source "https://jenkinsaspasssto.blob.core.windows.net/software/jre-8u191-windows-x64.exe" -Destination "D:\"
 
 $path = "C:\Program Files\Java"
 
