8a03dc72b212d18c8092ddf31f9a71bbc5218fa5
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-14 12:44:15 +0530

    Update jenkins.ps1

--- jenkins/jenkins.ps1
+++ jenkins/jenkins.ps1
@@ -1,3 +1,3 @@
-Start-BitsTransfer -Source "https://jenkinsaspasssto.blob.core.windows.net/software/jenkins.msi" -Destination "D:\"
-  
-Start-Process D:\jenkins.msi /qn -Wait
+Start-BitsTransfer -Source "https://jenkinsaspasssto.blob.core.windows.net/software/jenkins.msi" -Destination "D:\"  
+Start-Process D:\jenkins.msi /qn
+Start-Sleep 100
