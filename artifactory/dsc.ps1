58ab5513b4846582431959eb7a12e18612b851ac
Sangam <36110390+sangaml@users.noreply.github.com>
2018-11-18 11:10:25 +0530

    Update dsc.ps1

--- artifactory/dsc.ps1
+++ artifactory/dsc.ps1
@@ -4,14 +4,17 @@ Configuration CopyDSCModuleRemotely {
     
       Script Download-xActiveDirectory {  
         GetScript = {  
-          @{Result = Test-Path 'D:\cert\hotfix.zip'}  
+          @{Result = Test-Path 'D:\artifactory-oss-6.5.2.zip'}  
         }  
         SetScript = { 
-          Invoke-WebRequest -Uri 'https://csgdfe49495dc73x47efxabf.blob.core.windows.net/grt/artifactory-oss-6.5.2.zip' -OutFile 'D:\cert\hotfix.zip'  
+          Invoke-WebRequest -Uri 'https://csgdfe49495dc73x47efxabf.blob.core.windows.net/grt/artifactory-oss-6.5.2.zip' -OutFile 'D:\artifactory-oss-6.5.2.zip'  
+          Invoke-WebRequest -Uri 'https://jenkinsaspasssto.blob.core.windows.net/software/jre-8u191-windows-x64.exe' -OutFile 'D:\jre-8u191-windows-x64.exe'
+          Unblock-File -Path 'D:\jre-8u191-windows-x64.exe'
           Unblock-File -Path 'D:\artifactory-oss-6.5.2.zip'  
             
         }  
         TestScript = {  
+          Test-Path 'D:\jre-8u191-windows-x64.exe'
           Test-Path 'D:\artifactory-oss-6.5.2.zip'  
         }   
       } 
@@ -21,11 +24,20 @@ Configuration CopyDSCModuleRemotely {
         Destination = 'D:\'  
         DependsOn = '[Script]Download-xActiveDirectory'  
       }
+      Package InstallExe
+      {
+          Ensure          = "Present"
+          Name            = "Install Java"
+          Path            = "D:\jre-8u191-windows-x64.exe"
+          Arguments       = '/b"C:\Windows\Temp\PerforceClient" /S /V"/qn ALLUSERS=1 REBOOT=ReallySuppress"'
+          ProductId       = ''
+          DependsOn       = '[Script]Download-xActiveDirectory'
+      }
       Script Startartifactory {
         GetScript = {
           }  
           SetScript = { 
-            Invoke-Command -ScriptBlock 'D:\artifactory-oss-6.5.2\bin\run.vbs' 
+            Invoke-Command -ScriptBlock {D:\artifactory-oss-6.5.2\bin\run.vbs}
           }  
           TestScript = { 
           }
